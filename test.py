8c77a16bb1ef7ea8bca432107d8da7d17fd7deaa
minguk <mingukkim.data@gmail.com>
2021-11-21 14:19:07 +0900

    upload

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import win32com.client
 
 # test = win32com.client.Dispatch("CpSysDib.MarketEye")
-CpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
+# CpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
 # print(test.CodeToName('A005930'))
 # print(test.CodeToIndex('A005930'))
 # test.SetInputValue(0, [0,17,1,2,3,4,5])
@@ -9,4 +9,13 @@ CpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
 # test.BlockRequest()
 # print(test.GetDataValue(6,1))
 
-print(CpStockCode.NameToCode('삼성전자'))
+# print(CpStockCode.NameToCode('삼성전자'))
+
+
+test = win32com.client.Dispatch("Dscbo1.StockMst")
+test.SetInputValue(0, 'A005930')
+test.BlockRequest()
+print(test.GetHeaderValue(11))
+print(test.GetHeaderValue(13))
+print(test.GetHeaderValue(14))
+print(test.GetHeaderValue(15))
--- test_slack.py
+++ test_slack.py
@@ -7,6 +7,5 @@ def post_message(token, channel, text):
     )
     print(response)
  
-myToken = "xoxb-2723368580038-2727131983477-Vd6TIL7MwVUNqaIvw5yt2PSj"
  
 post_message(myToken,"#python-stock","hello slack!")
